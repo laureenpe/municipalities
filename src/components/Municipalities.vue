edd908d131a60ea74e43b2ac3ad3334b57f88a47
Laura <laureen.pelaez.isaza@gmail.com>
2019-07-02 15:09:24 +0200

    added autocomplete section

--- src/components/Municipalities.vue
+++ src/components/Municipalities.vue
@@ -2,6 +2,18 @@
   <div>
     <main>
       <h1 class="py-5">Trondheim Municipalities</h1>
+
+      <div class="container">
+        <div class="block-autocomplete">
+          <input
+            id="input"
+            type="autocomplete"
+            placeholder="Filter..."
+          >
+          <button>↩</button>
+        </div>
+      </div>
+
       <section>
         <article
           class="flex-element"
@@ -10,10 +22,12 @@
         >
           <div class="card">
             <div class="card-header">
-              <h1>{{item.description}} :<span>{{item.label}}</span></h1>
+              <h1>{{item.description}} :</h1>
+              <span>{{item.label}}</span>
             </div>
-            <div class="card-footer">
-              <span>Status: {{item.status}}</span>
+            <div class="card-footer animate">
+              <span>Status:</span>
+              <span> {{item.status}}</span>
             </div>
           </div>
         </article>
@@ -53,5 +67,60 @@ export default {
 .py-5 {
   padding-top: 2rem;
   padding-bottom: 2rem;
+}
+//Autocomplete
+
+.block-autocomplete {
+  position: relative;
+  margin: 1rem;
+  padding: 1rem;
+  left: 0;
+  top: 0;
+  background-color: white;
+  border-radius: 0;
+  font-size: 2rem;
+  overflow: hidden;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  max-width: 100%;
+
+  button {
+    background: transparent;
+    border: none;
+    outline: none;
+    font-size: 2rem;
+    padding-right: 2rem;
+    color: #c1c1c1;
+    transition: inherit;
+    flex: 0;
+    height: 2rem;
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+
+  &::focus-within,
+  ::active {
+    box-shadow: 0 8px 32px rgba(0, 0, 0, 0.16);
+  }
+
+  &:focus-within button {
+    color: green;
+  }
+
+  &:focus-within::before {
+    opacity: 1;
+  }
+}
+
+input[type="autocomplete"] {
+  font-size: 2rem;
+  border: none;
+  outline: none;
+  box-sizing: unset;
+  background: transparent;
+  opacity: 0.999;
+  flex: 1;
+  width: 100%;
 }
 </style>
