330129407f5ab7cfd0c273db3e6215c76973ecf0
Laura <laureen.pelaez.isaza@gmail.com>
2019-07-02 18:30:18 +0200

    added copy webpack to copy images from folders

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: 'src/assets/images',
+        to: 'assets/images/[path][name].[ext]',
+      }]),
+    ],
+
+  },
+  publicPath: process.env.NODE_ENV === 'production' ?
+    '/municipalities/' :
+    '/'
+
+};
